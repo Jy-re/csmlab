dfcc8e9475e937cf221c5885f9ef844e83ffaf7f
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2023-08-30 16:30:55 +0800

    Added styles and button for login page

--- Styles.js
+++ Styles.js
@@ -1,5 +1,60 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    
+
+    //Login Page Styles
+
+    bigLogo: {
+        marginBottom: 50,
+    },
+
+    screenContainer: {
+        flex: 1,
+        alignItems: 'center', // Center horizontally
+        justifyContent: 'center', // Center vertically
+    },
+
+    inputContainer: {
+        position: 'relative',
+        padding: 0,
+        margin: 0,
+        marginBottom: 10,
+    },
+
+    inputField: {
+        height: 50,
+        width: 300,
+        margin: 0,
+        paddingLeft: 30,
+        borderWidth: 1,
+    },
+
+    inputIcon: {
+        position: 'absolute',
+        bottom: 8,
+        bottom: 15,
+        right: 20,
+        width: 16,
+        height: 15
+        
+    },
+
+    buttonContainer: {
+        marginTop: 50,
+        paddingHorizontal: 135,
+        paddingVertical: 10,
+        borderRadius: 5,
+        backgroundColor: '#77C777',
+    },
+
+    buttonText: {
+        color: '#0A4610',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+
+    //End of Login Page Styles
+
 })
+
+export default styles;
--- components/pages/Login.js
+++ components/pages/Login.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Image, View, Text, TextInput, Button } from 'react-native';
+import { Image, View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import styles from '../../Styles.js'
 import Logo from '../../assets/imgs/LogoBig.png';
 
 export default function Login() {
   return (
-    <View>
-      <Image source={require('../../assets/imgs/LogoBig.png')} />
-      <Text>Login</Text>
-      <TextInput />
-      <TextInput />
-      <Button title="Login" />
+
+    //Note note: Sa android rani mudagan nga images. Dili siya basahon sa web kay native siya
+    <View style={styles.screenContainer}>
+      <Image style={styles.bigLogo} source={require('../../assets/imgs/LogoBig.png')} />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputField} placeholder='ID Number'/>
+        <Image style={styles.inputIcon} source={require('../../assets/imgs/UserLogo.png')}/>
+      </View>
+
+      <View style={styles.inputContainer}> 
+        <TextInput style={styles.inputField} placeholder='Password'/>
+        <Image style={styles.inputIcon} source={require('../../assets/imgs/SeePass.png')}/>
+      </View>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
