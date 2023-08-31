78b77bad3125bfe8aceb341835b18253d15e945c
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2023-08-31 16:38:30 +0800

    Added Borrow Form Header and Text (with issue)

--- Styles.js
+++ Styles.js
@@ -1,5 +1,69 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+
+    borrowFormPageContainer:{
+        flexDirection: "column",
+        alignItems: "flex-start",
+        width: 360,
+        paddingTop: 40,
+    },
+
+    borrowFormHeader: {
+        flex: 1,
+        paddingVertical: 0,
+        paddingHorizontal: 22,
+        justifyContent: "space-between",
+        alignItems: "center",
+        alignSelf: "stretch",
+        backgroundColor: 'yellow',
+    },
+
+    arrowContainer: {
+        marginRight: 'auto', // Pushes the arrow to the left
+    },
+
+    smallLogoContainer: {
+        marginLeft: 'auto', // Pushes the small logo to the right
+    },
+
+    body: {
+        flex: 1,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        alignSelf: 'stretch',
+    },
     
+    first: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        alignSelf: 'stretch',
+        marginVertical: 10, // For gap-like spacing, you can use marginVertical
+    },
+
+    title: {
+        flex: 1,
+        paddingVertical: 0,
+        paddingHorizontal: 7,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        alignSelf: 'stretch',
+    },
+
+    formTitle: {
+        color: '#1E1A4D',
+        fontSize: 20,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 20 * 1.5, // Adjust as needed based on your design
+    },
+
+    sampleText:{
+        fontSize: 20,
+    }
 })
+
+export default styles;
--- components/pages/BorrowForm.js
+++ components/pages/BorrowForm.js
@@ -6,12 +6,26 @@ export default function BorrowForm() {
     return(
         <View style={styles.borrowFormPageContainer}>
             <View style={styles.borrowFormHeader}>
-                <TouchableOpacity>
-                    <Image source={require('../../assets/imgs/Arrow.png')}></Image>
-                </TouchableOpacity>
-                <Image source={require('../../assets/imgs/SmallLogo.png')}></Image>
+                <View style={styles.arrowContainer}>
+                    <TouchableOpacity>
+                        <Image source={require('../../assets/imgs/Arrow.png')}></Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.smallLogoContainer}>
+                    <Image style={styles.smallLogo} source={require('../../assets/imgs/SmallLogo.png')}></Image>
+                </View>
             </View>
-            <Text>Whatevre</Text>
+
+            {/* This is for the body*/}
+            
+            <View style={styles.body}>
+                <View style={styles.first}>
+                    <View style={styles.title}>
+                        <Text style={styles.formTitle}>Fill-up Form</Text>
+                    </View>
+                </View>
+                
+            </View>
         </View>
     )
 }
